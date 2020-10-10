d76eb84b10c1768bef66cf42148114e0ad412919
李星星 <sss60@qq.com>
2020-10-10 19:18:24 +0800

    update

--- index.js
+++ index.js
@@ -2,48 +2,49 @@ const process = require('process');
 const express = require('express');
 const path  = require('path');
 const fs = require('fs');
+const urlencode = require('urlencode');
 
 const dir = process.cwd();
 const app = express();
 
-function renderIndex(req, resp) {
-    resp.sendFile(path.join(dir, 'index.html'));
-}
-
-function openMarkdown(req, resp) {
-    resp.send(fs.readFileSync(path.join(dir, req.query.path)))
-}
-
-function openDir(req, resp) {
+function listdir(dirname) {
     let lists = [];
 
-    files = fs.readdirSync(path.join(dir, req.query.path));
+    files = fs.readdirSync(path.join(dir, dirname));
     for(let i = 0; i < files.length; i++) {
         file = files[i];
 
-        stat = fs.statSync(path.join(dir, req.query.path, files[i]));
+        let fileinfo = {
+            filename: file, 
+            path: path.join(dirname, file),
+            is_file: true
+        };
 
+        stat = fs.statSync(path.join(dir, dirname, files[i]));
         if(stat.isDirectory())  {
-            lists.push({filename: file, is_dir: true});
-        } else {
-            lists.push({filename: file, is_dir: false, mtime: stat.mtime});
+            fileinfo['is_file'] = false;
         }
+
+        lists.push(fileinfo);
     }
 
-    resp.json(lists);
+    return lists;
 }
 
-app.get('/', function(req, resp){
-	const type = req.query.type
-
+app.get('/', function(req, resp) {
+    let type = req.query.type;
     switch(type) {
-        case 'open':
-            return openMarkdown(req, resp);
-        case 'open-dir':
-            return openDir(req, resp);
+        case 'listdir':
+            return resp.json(listdir(req.query.path));
+        case undefined:
+            return resp.sendFile(path.join(dir, 'index.html'));
         default:
-            return renderIndex(req, resp);
+            return resp.send('404 Not found.');
     }
+});
+
+app.get('*', function(req, resp) {
+    return resp.sendFile(path.join(dir, urlencode.decode(req.path)));
 });
 
 app.listen(3000);
