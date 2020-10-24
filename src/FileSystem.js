36b632e54e88f00d56f764c21f131a42f4ba7990
李星星 <sss60@qq.com>
2020-10-24 19:20:57 +0800

    update

--- src/FileSystem.js
+++ src/FileSystem.js
@@ -1,16 +1,17 @@
 const fs = require('fs');
 const path  = require('path');
+const pinyin = require("pinyin");
 
 /**
  * 获取目录的文件列表
  * @param  {string} dir 目录路径
- * @param { function } callback
+ * @param { function } filter
  * @param  {int} max_depth 最大遍历深度 0:无限
  * @param { only_dir } 只目录
  * @param  {int} depth 当前递归深度，
  * @return {object}
  */
-function list(dir, callback, max_depth = 0, only_dir = false, depth = 0) {
+function list(dir, filter, max_depth = 0, only_dir = false, depth = 0) {
     if(max_depth > 0 &&  depth + 1 > max_depth) {
         return [];
     }
@@ -27,12 +28,13 @@ function list(dir, callback, max_depth = 0, only_dir = false, depth = 0) {
             path: abspath.replace('\\', '/') + (stat.isDirectory() ? '/' : ''),
             is_file: stat.isDirectory() ? false : true,
             size: stat.size,
+            extname: stat.isDirectory() ? null : path.extname(abspath),
         };
 
-        if (callback) callback(fileinfo, file, abspath, stat, depth);
+        if (filter && !filter(fileinfo, file, abspath, stat, depth)) continue;
 
         if(stat.isDirectory()) {
-            fileinfo['children'] = list(abspath, callback, max_depth, only_dir, depth + 1);
+            fileinfo['children'] = list(abspath, filter, max_depth, only_dir, depth + 1);
         }
 
         if (!only_dir || stat.isDirectory()) {
@@ -43,55 +45,10 @@ function list(dir, callback, max_depth = 0, only_dir = false, depth = 0) {
     return results;
 }
 
-/**
- * 删除目录
- * @param  {string}  dir
- * @param  {Boolean} recursive 是否递归删除
- * @return {void}
- */
-function rmdir(dir, recursive = true) {
-    fs.rmdirSync(dir, {recursive: recursive});
-}
-
-/**
- * 删除文件
- * @param  {string}  file
- * @return {void}
- */
-function unlink(file) {
-    fs.unlinkSync(file);
-}
-
-/**
- * 删除文件
- * @param  {string}  file
- * @param  {string}  文件内容
- * @return {void}
- */
-function writeFile(file, content) {
-    let dirname = path.dirname(file);
-
-    if (!fs.existsSync(dirname)) {
-        mkdir(dirname, true);
-    }
-
-    fs.writeFileSync(file, content, {encoding: 'utf8'})
-}
-
-/**
- * 创建目录
- * @param  {string}  dirname 
- * @param  {Boolean} recursive 是否递归创建
- * @return {void}
- */
-function mkdir(dir, recursive = true) {
-    fs.mkdirSync(dir, {recursive: recursive, mode: 0o777})
-}
-
 module.exports = {
     list: list,
-    rmdir: rmdir,
-    unlink: unlink,
-    writeFile: writeFile,
-    mkdir: mkdir,
+    rmdir: fs.rmdir,
+    unlink: fs.unlink,
+    rename: fs.rename,
+    writeFile: fs.writeFile,
 };
--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ const bodyParser = require('body-parser');
 const urlencode = require('urlencode');
 const fileSystem = require('./FileSystem.js');
 
-function server(options) {
+function start(options) {
     options.root = path.resolve(options.root);
 
     /**
@@ -15,7 +15,7 @@ function server(options) {
      * @param  {string} name 目录或文件路径
      * @return {string} 目录或文件的绝对路径
      */
-    var resolve = (...name) => {
+    const resolve = (...name) => {
         let abspath = path.join(options.root, ...name);
         if (abspath.substr(0, options.root.length) !== options.root) {
             throw new Error(`非法的文件路径`);
@@ -24,85 +24,107 @@ function server(options) {
         return abspath;
     }
 
+    /**
+     * 验证用户授权
+     * @param  {req} req
+     * @return {boolean}
+     */
+    const auth = (req) => {
+        var authorization = req.header('authorization');
+        if(authorization) {
+            var account = Buffer.from(authorization.substr(6), 'base64').toString().split(':');
+            if(account[0] === options.username && account[1] === options.password) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    const routes = {
+        'list': (req, resp) => {
+            resp.json(fileSystem.list(resolve(req.body.path), (fileinfo, file, abspath, stat, depth) => {
+                fileinfo.path = fileinfo.path.substr(options.root.length);
+                return true;
+            }, req.body.depth, req.body.onlyDir));
+        },
+
+        'rmdir': (req, resp) => {
+            fileSystem.rmdir(resolve(req.body.path), req.body.options, err => {
+                if (err) throw err;
+                resp.json({});
+            });
+        },
+
+        'unlink': (req, resp) => {
+            fileSystem.unlink(resolve(req.body.path), err => {
+                if (err) throw err;
+                resp.json({});
+            });
+        },
+
+        'rename': (req, resp) => {
+            fileSystem.rename(resolve(req.body.path), resolve(req.body.newPath), err => {
+                if (err) throw err;
+                resp.json({});
+            });
+        },
+
+        'writeFile': (req, resp) => {
+            fileSystem.writeFile(resolve(req.body.path), req.body.content, req.body.options, err => {
+                if (err) throw err;
+                resp.json({});
+            });
+        }
+    };
+
+    /**
+     * express
+     * @type express
+     */
     const app = express();
 
     // 为项目设置密码
     if(options.username) {
         app.use(function(req, resp, next) {
-            var authorization = req.header('authorization');
-            if(authorization) {
-                var account = Buffer.from(authorization.substr(6), 'base64').toString().split(':');
-                if(account[0] === options.username && account[1] === options.password) {
-                    next()
-                    return;
-                }
+            if (auth(req)) {
+                next();
+                return;
             }
 
             resp.header('WWW-Authenticate', 'Basic realm="xxx"');
             resp.sendStatus(401);
-            return;
         });
     }
+
+    // 加载首页
+    app.get('/', function(req, resp) {
+        resp.sendFile(path.join(options.root, options.indexFile));
+    });
 
     app.use(express.static(options.root));
     app.use('/docsite/themes', express.static(path.join(__dirname, '../themes')));
     app.use('/docsite', express.static(path.join(__dirname, '../lib')));
     app.use(bodyParser.json({limit: '1mb'}));
-    app.use(bodyParser.urlencoded({
-        extended: true
-    }));
-
-    // 读取服务选项
-    app.get('/docsite/options', function(req, resp) {
-        resp.json({onlyRead: options.onlyRead});
-    });
-
-    // 加载首页
-    app.get('/', function(req, resp) {
-        resp.sendFile(path.join(options.root, 'index.html'));
-    });
+    app.use(bodyParser.urlencoded({extended: true}));
 
     // 功能接口
     app.post('/', function(req, resp) {
         let type = req.query.type;
-
-        switch(type) {
-            case 'list':
-                return resp.json(fileSystem.list(resolve(req.body.path), (fileinfo, file, abspath, stat, depth) => {
-                    fileinfo.path = fileinfo.path.substr(options.root.length);
-                }, req.body.depth || 0, req.body.only_dir || false));
-            case 'rmdir':
-                fileSystem.rmdir(resolve(req.body.path));
-                return resp.json({result: true});
-            case 'unlink':
-                fileSystem.unlink(resolve(req.body.path));
-                return resp.json({result: true});
-            default:
-                resp.status(404);
-                resp.json('404 Not found.');
+        if(routes[type] !== undefined) {
+            try {
+                routes[type](req, resp);
+            } catch(err) {
+                resp.status(500).json({message: err.message});
+            }
+        } else {
+            resp.status(404).send('404 Not found.');
         }
     });
-
-    if(!options.onlyRead) {
-        // 写入文件
-        app.post('*', function(req, resp) {
-            var data = '';
-            req.setEncoding('utf8');
-            req.on('data', function(chunk) {
-                data += chunk;
-            });
-
-            req.on('end', function() {
-                fileSystem.writeFile(resolve(urlencode.decode(req.path)), data);
-            });
-
-            resp.json({result: true});
-        });
-    }
 
     app.listen(options.port);
 }
 
 module.exports = {
-    server: server
+    start: start
 };
